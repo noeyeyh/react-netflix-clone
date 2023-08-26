ca4847752bc9dbe1f755a559b274cc3e709f19be
noeyeyh <ahy020606@naver.com>
2023-08-27 04:25:34 +0900

    Netflix 앱 완성하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import './App.css';
-import Banner from './components/Banner';
-import Nav from './components/Nav';
+import { Outlet, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import DetailPage from "./pages/DetailPage";
+import MainPage from "./pages/MainPage";
+import SearchPage from "./pages/SearchPage";
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Banner />
+    <div className="app">
+      <Routes> 
+        <Route path="/" element={<Layout />} >
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
